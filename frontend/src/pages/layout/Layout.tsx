10dccc2232548d8e6c38425f8e20e4115b344712
Freist Li <fauna179@hotmail.com>
2023-07-10 10:51:36 +0800

    Add one button to allow users add more facts

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -14,6 +14,10 @@ const Layout = () => {
         setIsSharePanelOpen(true);
     };
 
+    const handleAddFactsClick = () => {
+        window.open('https://forms.office.com/r/ZF6U7g8Egx', '_blank');
+    };
+
     const handleSharePanelDismiss = () => {
         setIsSharePanelOpen(false);
         setCopyClicked(false);
@@ -44,6 +48,10 @@ const Layout = () => {
                         <Link to="/" className={styles.headerTitleContainer}>
                             <h1 className={styles.headerTitle}>Copilot EAP</h1>
                         </Link>
+                        <div className={styles.AddFactsButtonContainer} role="button" tabIndex={0} aria-label="AddFacts" onClick={handleAddFactsClick} >
+                            <ShareRegular className={styles.shareButton} />
+                            <span className={styles.shareButtonText}>Add Facts</span>
+                        </div>
                         <div className={styles.shareButtonContainer} role="button" tabIndex={0} aria-label="Share" onClick={handleShareClick} onKeyDown={e => e.key === "Enter" || e.key === " " ? handleShareClick() : null}>
                             <ShareRegular className={styles.shareButton} />
                             <span className={styles.shareButtonText}>Share</span>
